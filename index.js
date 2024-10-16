bfe341fdd7aa52e7c1a8d3774d3c4849623a7663
OlaVasylieva7 <olhavas74@gmail.com>
2024-10-16 23:41:44 +0200

    update task 2

--- index.js
+++ index.js
@@ -27,4 +27,33 @@ Promise.all(minions)
     console.error("there is something wrong with one of the minions", error);
   });
 
+
+
+
 // task 2
+
+function randomMinionDelay(value) {
+  const delay = Math.floor(Math.random() * 4000) + 1000;
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`value: ${value}, delay: ${delay} ms`);
+    }, delay);
+  });
+}
+
+const minionss = [
+  randomMinionDelay("minion 1"),
+  randomMinionDelay("minion 2"),
+  randomMinionDelay("minion 3"),
+  randomMinionDelay("minion 4"),
+  randomMinionDelay("minion 5"),
+];
+
+Promise.race(minionss)
+  .then((result) => {
+    console.log("the fastest minion has finished its task ");
+    console.log(result);
+  })
+  .catch((error) => {
+    console.error("there is something wrong with one of the minions", error);
+  });
